2a2d0cb8bb294135c0313a21a1c91623a92263d0
Rubin Brovina <rbrovina@ritech.co>
2024-07-20 10:22:50 +0200

    Added ligh/dark theme switcher

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <v-app>
+  <v-app :dark="darkMode">
     <v-app-bar app>
       <v-toolbar-title>TrackTik challenge</v-toolbar-title>
+      <div class="themeWrapper">
+        <span>Theme</span>
+        <v-switch
+          :value="darkMode"
+          @change="toggleDarkMode"
+          class="themeSwitcher"
+        ></v-switch>
+      </div>
       <v-spacer></v-spacer>
       <div class="headerButtons">
         <v-btn text to="/">Client View</v-btn>
@@ -25,7 +33,14 @@ import Profile from "@/components/Profile.vue";
     Profile,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  darkMode = false;
+
+  toggleDarkMode(): void {
+    this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    this.darkMode = !this.darkMode;
+  }
+}
 </script>
 
 <style>
@@ -34,5 +49,14 @@ export default class App extends Vue {}
   justify-content: space-between;
   display: flex;
   align-items: center;
+}
+.themeWrapper {
+  align-items: center;
+  display: flex;
+  margin-left: 2rem;
+}
+.themeSwitcher {
+  margin-top: 1.3rem !important;
+  margin-left: 0.4rem;
 }
 </style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,20 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.blue.accent3,
+        secondary: colors.deepOrange.lighten1,
+      },
+      dark: {
+        primary: "#90CAF9",
+        secondary: "#E91E63",
+      },
+    },
+  },
+});
